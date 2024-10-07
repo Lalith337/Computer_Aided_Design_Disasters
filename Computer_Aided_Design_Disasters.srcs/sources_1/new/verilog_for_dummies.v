003f992be99fafe143cea0d8e9d140b119bcb0aa
num-byte <cipher0xx@gmail.com>
2024-10-07 20:18:07 +0530

    flipppy

--- Computer_Aided_Design_Disasters.srcs/sources_1/new/verilog_for_dummies.v
+++ Computer_Aided_Design_Disasters.srcs/sources_1/new/verilog_for_dummies.v
@@ -25,46 +25,46 @@
 
 //mux128_16
 module mux128_16 (
-  input wire [15:0] in0, 
-  input wire [15:0] in1, 
-  input wire [15:0] in2, 
-  input wire [15:0] in3, 
-  input wire [15:0] in4, 
-  input wire [15:0] in5, 
-  input wire [15:0] in6, 
-  input wire [15:0] in7, 
-  input wire [2:0] s, 
-  output reg [15:0] out
+    input  wire [15:0] in0,
+    input  wire [15:0] in1,
+    input  wire [15:0] in2,
+    input  wire [15:0] in3,
+    input  wire [15:0] in4,
+    input  wire [15:0] in5,
+    input  wire [15:0] in6,
+    input  wire [15:0] in7,
+    input  wire [ 2:0] s,
+    output reg  [15:0] out
 );
 
-  always @(*) begin
-    case (s)
-      3'b000: out = in0;
-      3'b001: out = in1;
-      3'b010: out = in2;
-      3'b011: out = in3;
-      3'b100: out = in4;
-      3'b101: out = in5;
-      3'b110: out = in6;
-      3'b111: out = in7;
-      default: out = 16'b0;
-    endcase
-  end
+    always @(*) begin
+        case (s)
+            3'b000:  out = in0;
+            3'b001:  out = in1;
+            3'b010:  out = in2;
+            3'b011:  out = in3;
+            3'b100:  out = in4;
+            3'b101:  out = in5;
+            3'b110:  out = in6;
+            3'b111:  out = in7;
+            default: out = 16'b0;
+        endcase
+    end
 
 endmodule
 
 //demux
 module demux8 (
-  input wire wr, 
-  input wire [2:0] s, 
-  output reg [7:0] load
+    input wire wr,
+    input wire [2:0] s,
+    output reg [7:0] load
 );
-  always @(*) begin
-    load = 8'b0;
-    if (wr) begin
-      load[s] = 1;
+    always @(*) begin
+        load = 8'b0;
+        if (wr) begin
+            load[s] = 1;
+        end
     end
-  end
 endmodule
 
 
@@ -76,7 +76,7 @@ module dfrl (
     input  wire d,
     output reg  q
 );
-    always @(posedge clk, reset) begin
+    always @(posedge clk or posedge reset) begin
         if (reset) q <= 1'b0;  // Reset output to 0
         else if (load) q <= d;  // Load data 'd' into 'q'
     end
@@ -85,58 +85,93 @@ endmodule
 
 //reg16 *shitty_name
 //use the looop lol and some *brain also
-module reg16(input wire clk, reset, load, input wire [15:0] d, output wire [15:0] q);
-  genvar j; //looping variable to over see looping same as int i; (in C)
-  
-  generate // generate block is used for conditional or iterative hardware generation at compile time
-    
-    for (j = 0; j < 16; j = j + 1) 
-    begin : dfrl_lol // (dfrl_lol)verilog requries a block name *takes care of the scope 
-      dfrl d_inst (.clk(clk), .reset(reset), .load(load), .d(d[i]), .q(q[i]));
-    end
-  
-  endgenerate // generate needs a end.
+module reg16 (
+    input wire clk,
+    reset,
+    load,
+    input wire [15:0] d,
+    output wire [15:0] q
+);
+    genvar j;  //looping variable to over see looping same as int i; (in C)
+
+    generate // generate block is used for conditional or iterative hardware generation at compile time
+
+        for (
+            j = 0; j < 16; j = j + 1
+        ) begin : dfrl_lol  // (dfrl_lol)verilog requries a block name *takes care of the scope
+            dfrl d_inst (
+                .clk(clk),
+                .reset(reset),
+                .load(load),
+                .d(d[j]),
+                .q(q[j])
+            );
+        end
+
+    endgenerate  // generate needs a end.
 
 endmodule
 
 
 //reg_file
 module reg_file (
-  input wire clk, 
-  input wire reset, 
-  input wire wr, 
-  input wire [2:0] rd_addr_a, 
-  input wire [2:0] rd_addr_b, 
-  input wire [2:0] wr_addr, 
-  input wire [15:0] d_in, 
-  output wire [15:0] d_out_a, 
-  output wire [15:0] d_out_b
+    input wire clk,
+    input wire reset,
+    input wire wr,
+    input wire [2:0] rd_addr_a,
+    input wire [2:0] rd_addr_b,
+    input wire [2:0] wr_addr,
+    input wire [15:0] d_in,
+    output wire [15:0] d_out_a,
+    output wire [15:0] d_out_b
 );
 
-  wire [7:0] load;
-  wire [15:0] r [0:7];
+    wire [7:0] load;
+    wire [15:0] r[0:7];
 
-  demux8 dmx (.wr(wr), .s(wr_addr), .load(load));
+    demux8 dmx (
+        .wr(wr),
+        .s(wr_addr),
+        .load(load)
+    );
 
-  genvar i;
-  generate
-    for (i = 0; i < 8; i = i + 1) begin : reg_loop
-      reg16 reg_inst (
-        .clk(clk),
-        .reset(reset),
-        .load(load[i]),
-        .din(d_in),
-        .r(r[i])
-      );
-    end
-  endgenerate
+    genvar i;
+    generate
+        for (i = 0; i < 8; i = i + 1) begin : reg_loop
+            reg16 reg_inst (
+                .clk(clk),
+                .reset(reset),
+                .load(load[i]),
+                .din(d_in),
+                .r(r[i])
+            );
+        end
+    endgenerate
 
-  mux128_16 mm0 (.in0(r[0]), .in1(r[1]), .in2(r[2]), .in3(r[3]), .in4(r[4]), .in5(r[5]), .in6(r[6]), .in7(r[7]), 
-                 .s(rd_addr_a), .out(d_out_a));
+    mux128_16 mm0 (
+        .in0(r[0]),
+        .in1(r[1]),
+        .in2(r[2]),
+        .in3(r[3]),
+        .in4(r[4]),
+        .in5(r[5]),
+        .in6(r[6]),
+        .in7(r[7]),
+        .s  (rd_addr_a),
+        .out(d_out_a)
+    );
 
-  mux128_16 mm1 (.in0(r[0]), .in1(r[1]), .in2(r[2]), .in3(r[3]), .in4(r[4]), .in5(r[5]), .in6(r[6]), .in7(r[7]), 
-                 .s(rd_addr_b), .out(d_out_b));
+    mux128_16 mm1 (
+        .in0(r[0]),
+        .in1(r[1]),
+        .in2(r[2]),
+        .in3(r[3]),
+        .in4(r[4]),
+        .in5(r[5]),
+        .in6(r[6]),
+        .in7(r[7]),
+        .s  (rd_addr_b),
+        .out(d_out_b)
+    );
 
 endmodule
-
-
